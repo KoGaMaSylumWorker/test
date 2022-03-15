3c5c3fcf531cd691e953bf46da95d7de7d66cce1
KoGaMaSylumWorker <101084885+KoGaMaSylumWorker@users.noreply.github.com>
2022-03-15 02:49:53 -0300

    Update testingfunc.js

--- testingfunc.js
+++ testingfunc.js
@@ -1,14 +1,11 @@
 // ==UserScript==
-// @name				        fewrdfc3werdfc
-// @namespace           http://tampermonkey.net/
-// @version			        3.3
-// @description	        kogama injector
-// @author			        Tuba
-// @match				        https://www.kogama.com/page/webgl-frame/*
-// @match				        https://kogama.com.br/page/webgl-frame/*
-// @match				        https://friends.kogama.com/page/webgl-frame/*
-// @grant				        none
-// @run-at			        document-start
+// @name         Autorenew novyků
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @author       You
+// @include      *okoun.cz/*
+// @icon         https://www.google.com/s2/favicons?domain=okoun.cz
+// @grant        none
 // ==/UserScript==
 console.log(this);
 'lol'
