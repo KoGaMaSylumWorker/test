6fd8eb1c27f706c4443a2be951727756e6166404
KoGaMaSylumWorker <101084885+KoGaMaSylumWorker@users.noreply.github.com>
2022-03-15 02:52:23 -0300

    Update and rename testingfuggjnc.js to testingfunc.user.js

---
+++ testingfunc.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name				fewrdfc3werdfc
+// @namespace           http://tampermonkey.net/
+// @version			    3.3
+// @description	        kogama injector
+// @author			    Tuba
+// @match				https://www.kogama.com/page/webgl-frame/*
+// @match				https://kogama.com.br/page/webgl-frame/*
+// @match				https://friends.kogama.com/page/webgl-frame/*
+// @grant				none
+// @run-at			    document-start
+// ==/UserScript==
+console.log(this);
