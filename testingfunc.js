9529461135d3528e3b9b8828f14de59c09937e0c
KoGaMaSylumWorker <101084885+KoGaMaSylumWorker@users.noreply.github.com>
2022-03-15 02:32:44 -0300

    Update testingfunc.js

--- testingfunc.js
+++ testingfunc.js
@@ -4,6 +4,7 @@
 // @version			3.3
 // @description	kogama injector
 // @author			Tuba
+ // @downloadURL  https://github.com/nickbarry/pd-helper-userscripts/raw/master/pd-statement-review-helper.user.js
 // @match				https://www.kogama.com/page/webgl-frame/*
 // @match				https://kogama.com.br/page/webgl-frame/*
 // @match				https://friends.kogama.com/page/webgl-frame/*
