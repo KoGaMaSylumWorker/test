52c17c28975a8bcb61f2f94d924ca44ea453397a
KoGaMaSylumWorker <101084885+KoGaMaSylumWorker@users.noreply.github.com>
2022-03-15 02:48:17 -0300

    Update testingfunc.js

--- testingfunc.js
+++ testingfunc.js
@@ -1,15 +1,13 @@
 // ==UserScript==
-// @name				fewrdfc3werdfc
-// @run-at			document-start
-// @version			3.3
-// @description	kogama injector
-// @namespace   lolmao
-// @author			Tuba
-// @downloadURL  https://github.com/KoGaMaSylumWorker/test/raw/main/testingfunc.js
-// @match				https://www.kogama.com/page/webgl-frame/*
-// @match				https://kogama.com.br/page/webgl-frame/*
-// @match				https://friends.kogama.com/page/webgl-frame/*
-// @grant				none
+// @name				        fewrdfc3werdfc
+// @namespace           http://tampermonkey.net/
+// @version			        3.3
+// @description	        kogama injector
+// @author			        Tuba
+// @match				        https://www.kogama.com/page/webgl-frame/*
+// @match				        https://kogama.com.br/page/webgl-frame/*
+// @match				        https://friends.kogama.com/page/webgl-frame/*
+// @grant				        none
+// @run-at			        document-start
 // ==/UserScript==
-
-top.console.log('lol');
+console.log(this);
